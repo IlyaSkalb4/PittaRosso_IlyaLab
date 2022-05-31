309003bac69635b0bb7467430c788b43c4f4df09
illy skalikxyb <pagany10@gmail.com>
2022-05-31 21:19:13 +0300

    add several function.

--- PittaRosso_IlyaLab/main.cpp
+++ PittaRosso_IlyaLab/main.cpp
@@ -1,9 +1,65 @@
 #include <iostream>
+#include <string>
+
+#define clear() system("cls")
 
 using namespace std;
 
+struct shoes
+{
+	char color;
+	int size;
+};
+struct seller
+{
+	string name;
+	int age;
+};
+struct shop
+{
+	string nameshop;
+	string addressshop;
+	seller salesman;
+	shoes pair;
+};
+
+shop* createShops(int size)
+{
+	shop* shops = new shop[size];
+	return shops;
+}
+void fillingShoe(shoes shoe)
+{
+	cout << "Enter seller name: ";
+	cin>>shoe.color;
+	cout << "Enter seller's age: ";
+	cin >> shoe.size;
+}
+void fillingSeller(seller salesman)
+{
+	cout << "Enter seller name: ";
+	getline(cin,salesman.name);
+	cout << "Enter seller's age: ";
+	cin >> salesman.age;
+}
+void fillingShops(int size, shop shop)
+{
+	cout << "Enter name shop: ";
+	getline(cin, shop.nameshop);
+	cout << "Enter address shop: ";
+	getline(cin, shop.addressshop);
+	fillingSeller(shop.salesman);
+	fillingShoe(shop.pair);
+}
+
 int main()
 {
+	int size = 0;
+	cout << "Enter number of shops: ";
+	cin >> size;
+	clear();
+	shop* shops = createShops(size);
 
+	delete[] shops;
 	return 0;
 }
